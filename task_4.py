05a49c4f1d55fa117dd5e1204ff880a9cb1773a1
nastya24704 <nastazeravina@gmail.com>
2025-03-23 19:42:11 +0700

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,14 @@
+coord = input()
+letter = coord[0]
+number = int(coord[1])
+
+if letter == 'a' or letter == 'c' or letter == 'e' or letter == 'g':
+    if number % 2 != 0:
+        print('черный')
+    else:
+        print('белый')
+else:
+    if number % 2 == 0:
+        print('черный')
+    else:
+        print('белый')
