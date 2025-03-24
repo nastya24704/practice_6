13ecbdf2e7df35f65c54d9ccd48424d0d9c1b103
nastya24704 <nastazeravina@gmail.com>
2025-03-24 10:05:27 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,1 +1,13 @@
-
+coord=input()
+letter1=coord[0]
+number1=int(coord[1])
+letter2=coord[3]
+number2=int(coord[4])
+letter_1=ord(letter1)-ord('a')+1
+letter_2=ord(letter2)-ord('a')+1
+if (abs(letter_1-letter_2)==1 and
+    abs(number1-number2)==2) or (abs(letter_1-letter_2)==2 and
+                                                     number1-number2==1):
+  print('верно')
+else:
+  print('ошибка')
